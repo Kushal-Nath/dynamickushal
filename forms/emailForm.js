d81ac993c56bad5bbd5ab727315f21493cb5aa01
Kushal-Nath <officialkushalnath2021@gmail.com>
2024-05-26 15:00:05 +0530

    first commit

---
+++ forms/emailForm.js
@@ -0,0 +1,20 @@
+// function submitForm() {
+//   // Retrieve form data
+//   var name = document.getElementById("name").value;
+//   var email = document.getElementById("email").value;
+//   var subject = document.getElementById("subject").value;
+//   var message = document.getElementById("message").value;
+
+//   // Construct email content
+//   var mailtoLink =
+//     "mailto:kushalnathking@gmail.com" +
+//     "?subject=" +
+//     encodeURIComponent(subject) +
+//     "&body=" +
+//     encodeURIComponent(
+//       "Name: " + name + "\nEmail: " + email + "\n\n" + message
+//     );
+
+//   // Open default email client with pre-filled email
+//   window.location.href = mailtoLink;
+// }
